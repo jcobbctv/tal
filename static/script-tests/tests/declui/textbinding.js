b6114dcbd74df934eda54bea6c17609b893ac825
jcobbctv <jcobbctv@gmail.com>
2013-11-13 13:01:56 +0000

    further changes

---
+++ static/script-tests/tests/declui/textbinding.js
@@ -0,0 +1,17 @@
+//(function() {
+//    this.TypeBindingTest = AsyncTestCase("TypeBindingTest");
+//
+//    this.TypeBindingTest.prototype.setUp = function() {
+//    };
+//
+//    this.TypeBindingTest.prototype.tearDown = function() {
+//    };
+//
+//    this.TypeBindingTest.prototype.testSingle = function(queue) {
+//        expectAsserts(1);
+//        queuedRequire(queue, ["antie/declui/declui"], function(DeclUI) {
+//
+//
+//        });
+//    };
+//})();
