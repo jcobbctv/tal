f5f6aa9746de2a2f421bc1611bc6ac132049d39c
jcobbctv <jcobbctv@gmail.com>
2013-11-15 10:15:02 +0000

    latest changes

--- static/script-tests/tests/declui/textbinding.js
+++ static/script-tests/tests/declui/textbinding.js
@@ -1,17 +1,32 @@
-//(function() {
-//    this.TypeBindingTest = AsyncTestCase("TextBindingTest");
-//
-//    this.TypeBindingTest.prototype.setUp = function() {
-//    };
-//
-//    this.TypeBindingTest.prototype.tearDown = function() {
-//    };
-//
-//    this.TypeBindingTest.prototype.testSingle = function(queue) {
-//        expectAsserts(1);
-//        queuedRequire(queue, ["antie/declui/declui"], function(DeclUI) {
-//
-//
-//        });
-//    };
-//})();
+(function() {
+    this.TypeBindingTest = AsyncTestCase("TextBindingTest");
+
+    this.TypeBindingTest.prototype.setUp = function() {
+    };
+
+    this.TypeBindingTest.prototype.tearDown = function() {
+    };
+
+    this.TypeBindingTest.prototype.testTextBindingHasNoInitFunction = function(queue) {
+        expectAsserts(1);
+        queuedRequire(queue, ["antie/declui/textbinding"], function(TextBinding) {
+
+            assertTrue( TextBinding.init === undefined );
+
+        });
+    };
+
+    this.TypeBindingTest.prototype.testTextBindingUpdateCallSetTextOnLabel = function(queue) {
+        expectAsserts(1);
+        queuedRequire(queue, ["antie/declui/textbinding"], function(TextBinding) {
+
+            var context = {
+                widget :
+
+            };
+
+
+        });
+    };
+
+})();
--- static/script/declui/textbinding.js
+++ static/script/declui/textbinding.js
@@ -9,16 +9,6 @@ require.def('antie/declui/textbinding',
 //            };
 
             update : function( binderParams ){
-                if( binderParams.context.widget.setText ){
-                    binderParams.context.widget.setText( binderParams.observable() );
-                }else{
-                    var i;
-
-                    for( i = 0; i < )
-
-
-                    var label = new Label( binderParams.observable() );
-                }
 
             }
         };
